853585bfa53a2a396497bd9772864036fcfb1e6b
MauroCasarin <70527971+MauroCasarin@users.noreply.github.com>
2026-03-02 12:28:27 -0300

    m

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import NewsFeed from './components/NewsFeed';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <NewsFeed />
+    </div>
+  );
+}
